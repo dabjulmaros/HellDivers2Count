47a7eaa53237dd00d7748f2e4beea0ed4e7f11bd
Dabjulmaros <33079720+dabjulmaros@users.noreply.github.com>
2024-03-06 11:29:36 -0500

    purge data

--- public/charts.js
+++ public/charts.js
@@ -1,3 +1,6 @@
+let chart, data;
+//Chart options constant defined in line 56
+
 document.addEventListener("htmx:afterRequest", function (evt) {
   const canvasEle = document.querySelector("canvas#playerChart");
   const playerCounts = document.querySelector("div.flex");
@@ -20,6 +23,35 @@ document.addEventListener("htmx:afterRequest", function (evt) {
     }).format(updatedDate);
   }
 });
+
+function createChart() {
+  const ctx = document.getElementById("playerChart");
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [],
+    },
+    options: chartOptions,
+  });
+}
+
+function updateChart(animate = true) {
+  chart.data.labels = [
+    ...data.map((e) => new Date(e["Updated"]).toLocaleString()),
+  ];
+  chart.data.datasets = [
+    {
+      label: "# of Steam Players",
+      data: data.map((e) => e["Count"]),
+      borderColor: "#ffe80a",
+      backgroundColor: "#ffe80a40",
+      fill: true,
+      tension: 0.5,
+    },
+  ];
+  chart.update(animate ? "" : "none");
+}
 
 const chartOptions = {
   elements: {
@@ -57,32 +89,3 @@ const chartOptions = {
     },
   },
 };
-let chart, data;
-function createChart() {
-  const ctx = document.getElementById("playerChart");
-  chart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: [],
-      datasets: [],
-    },
-    options: chartOptions,
-  });
-}
-
-function updateChart(animate = true) {
-  chart.data.labels = [
-    ...data.map((e) => new Date(e["Updated"]).toLocaleString()),
-  ];
-  chart.data.datasets = [
-    {
-      label: "# of Steam Players",
-      data: data.map((e) => e["Count"]),
-      borderColor: "#ffe80a",
-      backgroundColor: "#ffe80a40",
-      fill: true,
-      tension: 0.5,
-    },
-  ];
-  chart.update(animate ? "" : "none");
-}
